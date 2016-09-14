ae71bb7272bf31671bad2e9a501cbb948d13ff2a
jasonhillier <jason@paviasystems.com>
2016-09-14 16:39:19 -0700

    change to underscore hash

--- Docker-Publish.sh
+++ Docker-Publish.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 echo "Building docker image..."
-docker build . -t headlightsdk-app-{{AppHash-LowerCase}}
+docker build . -t headlightsdk_app_{{AppHash-LowerCase}}
 
 echo -n "Enter docker username to publish image:"
 read docker_user
 
 echo "Publishing docker image..."
-docker tag headlightsdk-app-{{AppHash-LowerCase}} $docker_user/headlightsdk-app-{{AppHash-LowerCase}}:latest
-docker push $docker_user/headlightsdk-app-{{AppHash-LowerCase}}:latest
+docker tag headlightsdk_app_{{AppHash-LowerCase}} $docker_user/headlightsdk_app_{{AppHash-LowerCase}}:latest
+docker push $docker_user/headlightsdk_app_{{AppHash-LowerCase}}:latest
